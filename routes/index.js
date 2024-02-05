4fd02d672bad273e6223123d00ea9e5d44f0ce7e
Hamza Mujinovic <haminimi.dev@gmail.com>
2024-02-05 20:27:39 +0100

    Add the error handler

--- routes/index.js
+++ routes/index.js
@@ -60,6 +60,15 @@ router.delete('/', (req, res) => {
 	res.redirect('/');
 });
 
+router.use((err, req, res, next) => {
+	console.error(err.stack);
+
+	res.locals.error = err;
+
+	res.status(err.status || 500);
+	res.render('error');
+});
+
 module.exports = {
 	router,
 };
